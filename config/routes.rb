e4085db50b4dc058b37e22a10d8f3e304cca129c
oscar navidad <oskrnav@gmail.com>
2019-11-05 11:25:59 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/index'
+  
+  namespace :api , :path => "", :constraints => {:subdomain => "api"} do
+    api_version(:module => "V1", :path => {:value => "v1"}) do
+      resources :users
+      post 'authenticate', to: 'authentication#authenticate'
+    end  
+  end
   resources :items
-  post 'authenticate', to: 'authentication#authenticate'
+  
 end
